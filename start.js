0e78d758c7f2cb1f39a5b1225e18fdec1c00eae2
Dipak Acharya <dpakach@gmail.com>
2019-02-11 16:31:23 +0545

    Add EsLint and npm script for linting

--- start.js
+++ start.js
@@ -5,9 +5,9 @@ const app = require('./app');
 const config = require('./config');
 
 // get port from config (3000 by default)
-const port = config.app.port;
+const { port } = config.app;
 
 // start the server and listen in given port
 http.createServer(app).listen(port, () => {
-	console.log(`Started Server in port ${port}`);
+  console.log(`Started Server in port ${port}`);
 });
