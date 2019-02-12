e3a13d6d059f58bd705d4d03086015ec0de63f66
Dipak Acharya <dpakach@gmail.com>
2019-02-12 20:49:54 +0545

    Add routes for user creation and password change

--- server/router/index.js
+++ server/router/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const { postUser, getUser, postUserPassword } = require('../controllers/userControllers');
+
 router.get('/ping', (req, res) => {
   res.send('Ping');
   res.sendStatus(200);
 });
+
+router.post('/user', postUser);
+router.get('/user/:id', getUser);
+router.post('/user/:id/password', postUserPassword);
 
 module.exports = router;
