d68e5a781ad3659c04a58eea218d1318f8e3f31f
Dipak <dpakach@gmail.com>
2019-03-18 16:01:34 +0545

    Add authenticate middleware to validate token sent in header

--- server/router/index.js
+++ server/router/index.js
@@ -7,18 +7,20 @@ const {
   postToken, getToken, deleteToken, putToken,
 } = require('../controllers/tokenController');
 
+const { authenticate } = require('../handlers/tokenHandlers');
+
 router.get('/ping', (req, res) => {
   res.send('Ping');
   res.sendStatus(200);
 });
 
 router.post('/user', postUser);
-router.get('/user/:id', getUser);
-router.post('/user/:id/password', postUserPassword);
+router.get('/user/:id', authenticate, getUser);
+router.post('/user/:id/password', authenticate, postUserPassword);
 
 router.post('/token', postToken);
-router.get('/token/:id', getToken);
-router.put('/token/:id', putToken);
-router.delete('/token/:id', deleteToken);
+router.get('/token/:id', authenticate, getToken);
+router.put('/token/:id', authenticate, putToken);
+router.delete('/token/:id', authenticate, deleteToken);
 
 module.exports = router;
