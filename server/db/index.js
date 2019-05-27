3fe80367bf4df158aae802f631ffd554836e4373
Dipak Acharya <dpakach@gmail.com>
2019-05-27 21:23:15 +0545

    Remove useless functions from dbHandler

--- server/db/index.js
+++ server/db/index.js
@@ -9,31 +9,6 @@ const dbHandler = {
 };
 
 /**
- * Construct query params from the object
- *
- * @param Object obj
- *
- * @return array[String keys, Array values]
- */
-dbHandler.constructQuery = function (obj) {
-  const keys = Object.keys(obj);
-  const values = keys.map(k => obj[k]);
-  return [keys.join(','), values];
-};
-
-/**
- * Get placeholder test for query for given values array
- * ['id', 'username'] => [$1, $2]
- *
- * @param Array values
- *
- * @return Array
- */
-dbHandler.getValuePlaceholder = function (values) {
-  return values.map((_, i) => `$${i + 1}`);
-};
-
-/**
  * dbHandler for creating an object
  *
  * @param string tableName
@@ -42,8 +17,10 @@ dbHandler.getValuePlaceholder = function (values) {
  * @return Promise
  */
 dbHandler.dbCreate = function (tableName, data) {
-  const [keys, values] = dbHandler.constructQuery(data);
-  const queryText = `INSERT INTO "${tableName}" (${keys}) VALUES(${dbHandler.getValuePlaceholder(values)});`;
+  const keys = Object.keys(data).join(',');
+  const values = Object.keys(data).map(k => data[k]);
+  const valuesPlaceholder = values.map((_, i) => `$${i + 1}`).join(',');
+  const queryText = `INSERT INTO "${tableName}" (${keys}) VALUES(${valuesPlaceholder});`;
   return dbHandler.query(queryText, values);
 };
 
@@ -70,9 +47,9 @@ dbHandler.dbRemove = function (tableName, id) {
  * @return Promise
  */
 dbHandler.dbUpdate = function (tableName, id, data) {
-  const values = dbHandler.constructQuery(data)[1];
-  const updateValues = Object.keys(data).map((k, i) => `${k}=$${i + 1}`);
-  const queryText = `UPDATE "${tableName}" SET ${updateValues.join(',')} where id like $${updateValues.length + 1}`;
+  const values = Object.keys(data).map(k => data[k]);
+  const updateValues = Object.keys(data).map((k, i) => `${k}=$${i + 1}`).join(',');
+  const queryText = `UPDATE "${tableName}" SET ${updateValues} where id like $${values.length + 1}`;
   return dbHandler.query(queryText, [...values, id]);
 };
 
@@ -86,11 +63,12 @@ dbHandler.dbUpdate = function (tableName, id, data) {
  * @return Promise
  */
 dbHandler.dbUpdateSelector = function (tableName, selectors, data) {
-  const values = dbHandler.constructQuery(data)[1];
-  const updateValues = Object.keys(data).map((k, i) => `${k} = $${i + 1}`);
+  const updateValues = Object.keys(data).map(k => data[k]);
+  const selectorValues = Object.keys(selectors).map(k => selectors[k]);
+  const updateText = Object.keys(data).map((k, i) => `${k} = $${i + 1}`).join(',');
   const selectorText = Object.keys(selectors).map((s, i) => `${s} = $${i + 1 + updateValues.length}`).join(' and ');
-  const queryText = `UPDATE "${tableName}" SET ${updateValues.join(',')} where ${selectorText};`;
-  return dbHandler.query(queryText, values.concat(Object.keys(selectors).map(k => selectors[k])));
+  const queryText = `UPDATE "${tableName}" SET ${updateText} where ${selectorText};`;
+  return dbHandler.query(queryText, updateValues.concat(selectorValues));
 };
 
 /**
@@ -118,21 +96,21 @@ dbHandler.dbRead = function (tableName, id, values) {
  *
  * @param string tableName
  * @param Object selectors
- * @param Array selectorValues
+ * @param Array values
  *
  * @return Promise
  */
-dbHandler.dbReadSelectors = function (tableName, selectors, selectorValues) {
+dbHandler.dbReadSelectors = function (tableName, selectors, values) {
   let queryValues = '';
-  if (!selectorValues) {
+  if (!values) {
     queryValues = '*';
   } else {
-    queryValues = selectorValues.join(',');
+    queryValues = values.join(',');
   }
-  const values = Object.keys(selectors).map(k => selectors[k]);
+  const selectorValues = Object.keys(selectors).map(k => selectors[k]);
   const selectorText = Object.keys(selectors).map((s, i) => `${s} LIKE $${i + 1}`).join(' AND ');
   const queryText = `SELECT ${queryValues} FROM ${tableName} WHERE ${selectorText}`;
-  return dbHandler.query(queryText, values);
+  return dbHandler.query(queryText, selectorValues);
 };
 
 // Export the handler
